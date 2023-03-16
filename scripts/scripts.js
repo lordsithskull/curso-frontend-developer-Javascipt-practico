92345f570a0ffa7732b684e35dbe368894ff0a7b
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2023-03-16 01:35:56 -0600

    Se oculta menu mobile, y se agrega el shopping cart

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const dektopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
+menuHamIcon.addEventListener('click',toggleMobileMenu)
 
 function toggleDesktopMenu () {
     dektopMenu.classList.toggle('inactive');
 }
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
+}
+
