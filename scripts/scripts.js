fbd412332d501fe31562c37fffe91a42907fc67a
Miguel Sánchez Escalera <miguelse2@hotmail.com>
2023-03-27 19:39:52 -0600

    Menu carrito agregado y se genera JS para activar desactivar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,16 @@ const menuEmail = document.querySelector('.navbar-email');
 const dektopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+
 
 menuEmail.addEventListener('click',toggleDesktopMenu)
 menuHamIcon.addEventListener('click',toggleMobileMenu)
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 
+/*funciones de menu*/
 function toggleDesktopMenu () {
     dektopMenu.classList.toggle('inactive');
 }
@@ -14,3 +20,6 @@ function toggleMobileMenu () {
     mobileMenu.classList.toggle('inactive');
 }
 
+function toggleCarritoAside () {
+    aside.classList.toggle('inactive');
+}
